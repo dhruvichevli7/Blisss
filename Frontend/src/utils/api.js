d889aa35be8de2449b88da51bdb6c6b071942544
dhruvichevli7 <dhruvichevli7@gmail.com>
2026-04-17 11:39:41 +0530

    api update

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://blisss-ptkv.onrender.com",
 });
 
 // Attach token automatically
